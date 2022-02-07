669419c8ce79ab3f2e36e3c968d9e3e4b24fc9da
Lucas <lucasrios7987@gmail.com>
2022-02-07 02:45:08 -0300

    vistazo de menu para final

---
+++ menu.py
@@ -0,0 +1,18 @@
+import os
+
+
+def Menu () :
+    print("-------------------------------")
+    print("""
+    Seleccione una opción...
+    1 - Agregar estudiante
+    2 - Buscar estudiante por nombre
+    3 - Modificar nota
+    4 - Listado ordenados por nombre
+    5 - Listado ordenados por notas
+    6 - Mostrar promedio de las notas
+    7 - Borrar un estudiante
+    8 - Calcular la edad promedio de los estudiantes
+    0 - Salir """)
+    print("-------------------------------")
+    
